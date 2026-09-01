85daf9e1ad2d9f98ef9e2152094bb9ef4c8f9913
Filb3r <vilpuri997@gmail.com>
2026-09-01 15:34:14 +0300

    S1-09: Made react vite frontend and made empty site.

---
+++ frontend/README.md
@@ -0,0 +1,9 @@
+# Frontend
+
+## Starting command:
+
+1. Development mode
+```bash
+npm run dev
+```
+
---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+        <div>
+          <h1>Aloitus sivu</h1>
+          <p>
+            Tulossa....
+          </p>
+        </div>
+    </>
+  )
+}
+
+export default App
