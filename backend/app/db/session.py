3c0d1db87f3d3e489cb3ec0f787e6fe31bf34bf0
Filb3r <vilpuri997@gmail.com>
2026-09-07 18:31:12 +0300

    S2-01: Added SQLAlchemy engine and made a session for it. Also added base in db/base.py

---
+++ backend/app/db/session.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.core.config import Settings
+
+settings = Settings()
+
+engine = create_engine(
+    settings.database_url,
+    pool_pre_ping=True
+    )
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
